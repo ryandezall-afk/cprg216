5564b032f48436fb2869c8c75053680bfd890b8a
Ryan Dezall <ryandezall@gmail.com>
2025-09-18 16:05:39 -0600

    Week 3 final class part 1 casting

--- week3.py
+++ week3.py
@@ -18,3 +18,26 @@ print(b, type(b))
 print(c, type(c))
 print("Hello my name is", user_input, type(user_input))
 print("THe value of x * y is:", x*y, "and the type of x is:", type(x), "and the type of y is:", type(y))
+
+# some functions call : print, input, int, float, str, bool,
+
+#&&&&&&&&&&&&&
+# casting (changing one type into another, ex. int - float)
+# when we do it manually we call it explicit casting
+# changing int to float is increasing size (promotion)
+# int size is 4 bytes, float is 8 bytes
+#&&&&&&&&&&&&&
+
+num_a_text = "43"
+num_as_num = int(num_a_text)
+
+print(num_a_text, type(num_a_text))
+print(num_as_num)
+
+num = 3
+num_f = float(3)
+
+num2 = 3.4
+num2_i = int(num2)
+
+num2_as_text = str(num2)
