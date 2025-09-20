5729d0fdbff6ece4882f9d2854d4abf3b5028039
Ryan Dezall <ryandezall@gmail.com>
2025-09-19 19:11:27 -0600

    saving before work

--- user_input.py
+++ user_input.py
@@ -1,4 +1,10 @@
+#Determining the area of a circle 
+# 3.14 and 2 are already pre-determined 
+# user inputs radius and area will result
+
 user_input = input("Pleasse enter the radius of the circle\n")
-area = user_input * 2 * 3.14
-print("The area of the circle is:", area , type(area))
+area = int(user_input) * 2 * 3.14
+print("The area of the circle is:", area)
+
+
 
